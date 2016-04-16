6f022643499fa785eb7e4ef8ca746cb809dd71bd
R0MAR!0 $!LVA <R0MAR!0 $!LVA>
2016-04-16 13:27:00 -0300

    Adicionado tela de inserir frequencia

--- admin/addHorario.php
+++ admin/addHorario.php
@@ -4,54 +4,56 @@
   include '../classes/notas.class.php'; 
   Funcoes::geraHeader();
   Funcoes::geraMenus();
-  $dados = (Object) $_POST;
-  $objetoNotas = new Notas();
-  if(count($dados) && isset($dados->acao) && $dados->acao == "Salvar") {
-    $itens['cd_aluno'] = $dados->cd_aluno;
-    $itens['cd_turma'] = $dados->cd_turma;
-    $itens['nota1'] = $dados->nota1;
-    $itens['nota2'] = $dados->nota2;
-    $itens['status'] = $dados->status;
-    $objetoNotas->insereNotas($itens);
-  }
-?>
+  ?>
 <form action="addHorario.php" method="post">
 
   <div class="row">
     <div class="col-md-12">
-      <h4> <strong> Curso:</strong> Administrador de Redes e SQL Server com Assistência Técnica e Design </h4>
+      <div class="form-group">
+          <label for="exampleInputEmail1">Selecione o curso:</label>
+          <select class="form-control" name="cd_curso" >
+          <option value="1">ADM. e SQL com assistência técnica e design</option>
+          <option value="2">Costura</option>
+          <option value="2">Eletricista</option>
+          <option value="2">Gastronomia</option>
+          <option value="2">Construção Cívil</option>
+          <option value="2">Costura</option>
+          <option value="2">Costura</option>
+          <option value="2">Costura</option>
+        </select>
+        </div>
     </div>
     <table class="table table-hover">
   <tbody>
-    <div>
     <tr>
       <td class="text-center"> Data </td> 
       <td class="text-center"> Horário </td> 
       <td class="text-center"> Componente </td> 
     </tr>
-   </div>
-  </div>
-    <?php
-      $objetoAluno = new Aluno();
-      $alunos = $objetoAluno->getAlunos();
-      foreach ($alunos as $aluno) :
-      $notas = $objetoNotas->getNotas($aluno->cd_aluno);
-    ?>
+    <?php for($i = 0; $i < 12; $i++) : ?>
       <tr>
         <td class="col-md-2 text-center">
-          <input type="text" maxlength="3" class="form-control notas" data-id="<?= $aluno->cd_aluno; ?>" name="nota2<?= $aluno->cd_aluno; ?>"   value="<?= (isset($notas[0])) ? $notas[0]->nota2 : '' ; ?>">
+          <input type="date" >
         </td>  
         <td class="col-md-2 text-center">
-          <input type="text" maxlength="3" data-id="<?= $aluno->cd_aluno; ?>" class="form-control notas" name="nota1<?= $aluno->cd_aluno; ?>"  value="<?= (isset($notas[0])) ? $notas[0]->nota1 : '' ; ?>">
+          <input type="time" >
         </td> 
         <td class="col-md-2 text-center">
-          <input type="text" maxlength="3" class="form-control notas" data-id="<?= $aluno->cd_aluno; ?>" name="nota2<?= $aluno->cd_aluno; ?>"  value="<?= (isset($notas[0])) ? $notas[0]->nota2 : '' ; ?>">
+          <input type="text" >
         </td> 
       </tr>
+    <?php endfor; ?>
+       
       </tbody>
-    <?php endforeach; ?>
   </table>
-  <input type="hidden" value="Salvar " name="acao"></input>
-  <button type="submit" class="btn btn-primary btn-block">Salvar</button>
+  <div class="row">
+      <div class="col-md-4 pull-right">
+         <input type="reset"  name="botao"  class="btn btn-primary btn-block" value="Limpar"></input> 
+      </div>
+       <div class="col-md-4 pull-right">
+          <input type="hidden" value="Salvar" name="acao"></input>
+          <button type="submit" class="btn btn-primary btn-block">Salvar</button>
+       </div>
+ </div>
 </form>
 <?php Funcoes::geraFooter("notas"); ?>
--- admin/verNotas3.php
+++ admin/verNotas3.php
@@ -15,18 +15,32 @@
     $objetoNotas->insereNotas($itens);
   }
 ?>
-<form action="verNotas2.php" method="post">
+<form action="verNotas.php" method="post">
 
   <div class="row">
     <div class="col-md-12">
       <h4> <strong> Curso:</strong> Administrador de Redes e SQL Server com Assistência Técnica e Design </h4> 
     </div>
   </div>
-
-  <table class="table table-hover">
-  <tbody>
+  <div class="row">
+    <div class="col-md-6">
+      <h4> <strong>Professor:</strong> José Souza da Costa de Jesus</h4> 
+    </div>
+    <div class="col-md-3">
+      <h4> <strong>Código:</strong> 13363  </h4> 
+    </div>
+    <div class="col-md-3">
+      <h4> <strong> Carga Horária:</strong> 100  </h4> 
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h4> <strong> Disciplina:</strong> Web Design I </h4> 
+    </div>
+  </div>
+  <table class="table table-bordered">
     <tr>
-      <td> Disciplina </td> 
+      <td> Nome </td> 
       <td class="text-center"> Nota 1 </td> 
       <td class="text-center"> Nota 2 </td> 
       <td class="text-center"> Status </td> 
@@ -55,7 +69,6 @@
           <input type="hidden" name="status" data-id="<?= $aluno->cd_aluno; ?>" value="<?= (isset($notas[0]) && $notas[0]->status) ? 1 : 0; ?>">
         </td> 
       </tr>
-      </tbody>
     <?php endforeach; ?>
   </table>
   <input type="hidden" value="Salvar" name="acao"></input>
