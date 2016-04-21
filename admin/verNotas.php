59e767fb192dea6f95d807a4220125c81600d33f
EricaRenata <ericarenata879@gmail.com>
2016-04-21 15:30:51 -0300

    Refatorado Ver notas

--- admin/verNotas.php
+++ admin/verNotas.php
@@ -12,33 +12,32 @@
       <h4> <strong> Curso:</strong> Administrador de Redes e SQL Server com Assistência Técnica e Design </h4>
     </div>
   </div>
-  <div class="row">
+ <div class="row">
     <div class="col-md-12">
   <table class="table table-hover">
-    <thead>
-      <tr>
-        <td class="text-left"> Disciplina </td> 
-        <td class="text-center"> Nota 1 </td> 
-        <td class="text-center"> Nota 2 </td> 
-        <td class="text-center"> Status </td> 
-      </tr>
-    </thead>
+   <thead>
+    <tr>
+      <td class="text-left"> Disciplina </td> 
+      <td class="text-center"> Nota 1 </td> 
+      <td class="text-center"> Nota 2 </td> 
+      <td class="text-center"> Status </td> 
+   </thead>
+    </tr>
       <tbody>
       <?php for($i = 0; $i < 12; $i++) : ?>
-        <tr>
-          <td>&nbsp;</td>  
-          <td>&nbsp;</td>
-          <td>&nbsp;</td> 
-          <td>&nbsp;</td> 
-        </tr>
+      <tr>
+        <td>&nbsp;</td>  
+        <td>&nbsp;</td>
+        <td>&nbsp;</td> 
+        <td>&nbsp;</td> 
+      </tr>
       <?php endfor; ?>
       </tbody>
   </table>
     </div>
     <div class="col-md-6">
-      <input type="button"  name="imprimir"   class="btn btn-primary btn-block" value="Imprimir" onclick="window.print()";>
-    </div>
-  </div>
+      <input type="button" name="imprimir" class="btn btn-primary btn-lg btn-block" value="Imprimir" onclick="window.print();">
+ </div>
 
 
 </form>
