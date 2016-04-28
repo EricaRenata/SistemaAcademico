7962737868a1adeb29a0c6bc40592f8f050b99f2
EricaRenata <ericarenata879@gmail.com>
2016-04-27 21:25:57 -0300

    Correção de login.class.php melhorando a peformace e segurança

--- classes/login.class.php
+++ classes/login.class.php
@@ -2,26 +2,35 @@
 Class LoginUsuario extends Funcoes {
 	
 	public function verificaLogin ($matricula, $senha){
+		$aluno = $this->verificaAluno($matricula, $senha);
 		$profissional = $this->verificaProfissional($matricula, $senha);
-		if(!count($profissional) || $profissional == NULL) {
-			$objetoSqln = new Database();
-			$getadmin = $objetoSqln->query('select * from cad_admin where login = '.$matricula.' AND senha = 123456 ')->row();
-			if(count($getadmin) > 0) {
-				$this->iniciaSessao($getadmin['0']);
-				header("Location: admin");
-			}else {
-				$objetoSql = new Database();
-			$getaluno = $objetoSql->query("select * from seg_usuario inner join cadaluno using(cd_aluno) where login = {$matricula}	AND senha = {$senha}")->result();
-				if(count($getaluno) > 0) {
-					$this->iniciaSessao($getaluno['0']);
-				}else {
-					Self::setMensagem('Usuário ou Senha Inválido');	
-				}
-			}
-		} else {
-			$this->iniciaSessao($profissional, true);
+		$administrador = $this->verificaAdministrador($matricula, $senha);
+
+		if($aluno) {
+			$this->iniciaSessao($aluno, false, true);
 			header("Location: admin");
+		} else if($profissional) {
+			$this->iniciaSessao($profissional, true, false);
+			header("Location: admin");
+		} else if($administrador) {
+			$this->iniciaSessao($administrador);
+			header("Location: admin");
+		} else {
+			Self::setMensagem('Usuário ou Senha Inválido');
 		}
+	}
+
+	public function verificaAluno($matricula, $senha) {
+		$objetoSql = new Database();
+		$getlogin = $objetoSql->query("select * from seg_usuario inner join cadaluno using(cd_aluno) where login = {$matricula}	AND senha = {$senha}")->result();
+		return (count($getlogin) > 0 ) ? $getlogin[0] : false;
+	}
+
+
+	public function verificaAdministrador($matricula, $senha) {
+		$objetoSql1 = new Database();
+		$getlogin = $objetoSql1->query("select * from cad_admin where login = '".$matricula."' AND senha = {$senha}")->result();
+		return (count($getlogin) > 0 ) ? $getlogin[0] : false;
 	}
 
 	public function verificaProfissional($matricula, $senha) {
@@ -30,22 +39,13 @@ Class LoginUsuario extends Funcoes {
 		return (count($getlogin) > 0 ) ? $getlogin[0] : false;
 	}
 
-	public function iniciaSessao($dados, $profissional = false) {
+	public function iniciaSessao($dados) {
 		session_start();
-		if($profissional) {
-			$_SESSION['login'] = $dados->login;
-			$_SESSION['logado'] = true;
-			$_SESSION['cd_usuario'] = $dados->cd_usuario;
-			$_SESSION['cd_prof'] = $dados->cd_prof;
-			$_SESSION['nome'] = $dados->razao_social;	
-			$_SESSION['super'] = false;	
-		}else {
-			$_SESSION['login'] = $dados->login;
-			$_SESSION['logado'] = true;
-			$_SESSION['cd_usuario'] = $dados->cd_usuario;
-			$_SESSION['cd_aluno'] = ($dados->cd_aluno) ? $dados->cd_usuario : null;
-			$_SESSION['nome'] = $dados->razao_social;
-			$_SESSION['super'] = true;	
-		}
+		$_SESSION['login'] = $dados->login;
+		$_SESSION['logado'] = true;
+		$_SESSION['cd_usuario'] = $dados->cd_usuario;
+		$_SESSION['cd_prof'] = (isset($dados->cd_prof)) ? $dados->cd_prof : false;
+		$_SESSION['nome'] = $dados->razao_social;	
+		$_SESSION['super'] = (isset($dados->cd_admin)) ? $dados->cd_admin : false;	
 	}
 }
