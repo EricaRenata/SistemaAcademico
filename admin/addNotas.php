555b90f597ab9a75072751819e60d263480e44c2
Tamara Carvalho <Tamara Carvalho>
2016-04-30 16:59:16 -0300

    foi feita alterações em notas, e em outros arquivos, criando botoes, inputs e select etc

--- admin/addNotas.php
+++ admin/addNotas.php
@@ -2,38 +2,63 @@
   include '../classes/funcoes.class.php'; 
   include '../classes/aluno.class.php'; 
   include '../classes/notas.class.php'; 
+  include '../classes/curso.class.php'; 
+  include '../classes/disciplina.class.php'; 
   Funcoes::geraHeader();
   Funcoes::geraMenus();
-  
   $dados = (Object) $_POST;
+
   $objetoNotas = new Notas();
   if(count($dados) && isset($dados->acao) && $dados->acao == "Salvar") {
     $objetoNotas->insereNotas($dados);
   }
 ?>
 <form action="addNotas.php" method="post">
+<div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">
+      <form method="POST" action="addNotas.php" name="consulta">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Selecione o curso</label>
+          <select class="form-control" name="cd_curso" onchange="submit()">
+            <?php
+              $objetoCurso = new Curso();
+                $listaCurso = $objetoCurso->getCursos();
+                foreach ($listaCurso as $curso) :
+                  if($dados->cd_curso == $curso->cd_curso) :
+            ?>
+                    <option value="<?= $curso->cd_curso ?>" selected><?= $curso->nome_curso; ?></option>
+            <?php 
+                  else :
+            ?>
+                  <option value="<?= $curso->cd_curso ?>"><?= $curso->nome_curso; ?></option>
+            <?php 
+                  endif; 
+                endforeach; 
+            ?>
+          </select>
+        </div>
+      <div class="form-group">
+              <label for="exampleInputEmail1">Selecione a Disciplina:</label>
+                <select class="form-control" name="cd_disciplina" onchange="submit()">
+                  <option>Selecione a Disciplina</option>
+                <?php 
 
-  <div class="row">
-    <div class="col-md-12">
-      <h4> <strong> Curso:</strong> Administrador de Redes e SQL Server com Assistência Técnica e Design </h4> 
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-6">
-      <h4> <strong>Professor:</strong> José Souza da Costa de Jesus</h4> 
-    </div>
-    <div class="col-md-3">
-      <h4> <strong>Código:</strong> 13363  </h4> 
-    </div>
-    <div class="col-md-3">
-      <h4> <strong> Carga Horária:</strong> 100  </h4> 
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <h4> <strong> Disciplina:</strong> Web Design I </h4> 
-    </div>
-  </div>
+                  if(isset($dados->cd_curso) && $dados->cd_curso != 0) {
+                    $objetoDisciplina = new Disciplina();
+                    $listaDisciplina = $objetoDisciplina->getDisciplinas($dados->cd_curso);
+                    foreach ($listaDisciplina as $disciplina) :
+                      if($dados->cd_disciplina == $curso->cd_disciplina) : ?>
+                          <option value="<?= $curso->cd_curso ?>" selected><?= $disciplina->nome_disciplina; ?></option>
+                <?php else : ?>
+                        <option value="<?= $curso->cd_curso ?>"><?= $disciplina->nome_disciplina; ?></option>
+                <?php 
+                      endif; 
+                    endforeach; 
+                  }
+                ?>
+              </select>
+          </div>
+    </div>    
   <table class="table table-bordered">
     <tr>
       <td> Nome </td> 
