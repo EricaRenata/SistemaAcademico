64c9a14b7005190988e7e78c94df8c3b85df67ab
R0MAR!0 $!LVA <R0MAR!0 $!LVA>
2016-05-01 04:17:40 -0300

    Atualizacao do BD

--- sga.sql.sql
+++ sga.sql.sql
@@ -1,11 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
+﻿-- phpMyAdmin SQL Dump
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 01-Maio-2016 às 07:51
--- Versão do servidor: 5.6.26
--- PHP Version: 5.5.28
+-- Generation Time: 01-Maio-2016 às 09:11
+-- Versão do servidor: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `sga`
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `arquivos` (
-  `cd_arquivo` int(11) NOT NULL,
+`cd_arquivo` int(11) NOT NULL,
   `nome` varchar(520) NOT NULL,
   `cd_usuario` bigint(20) NOT NULL,
   `dt_sistema` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -50,7 +50,7 @@ INSERT INTO `arquivos` (`cd_arquivo`, `nome`, `cd_usuario`, `dt_sistema`, `cd_cu
 --
 
 CREATE TABLE IF NOT EXISTS `cadaluno` (
-  `cd_aluno` bigint(45) NOT NULL,
+`cd_aluno` bigint(45) NOT NULL,
   `razao_social` varchar(125) NOT NULL,
   `matricula` bigint(45) NOT NULL,
   `cd_turma` bigint(45) NOT NULL
@@ -75,7 +75,7 @@ INSERT INTO `cadaluno` (`cd_aluno`, `razao_social`, `matricula`, `cd_turma`) VAL
 --
 
 CREATE TABLE IF NOT EXISTS `cad_admin` (
-  `cd_admin` bigint(20) NOT NULL,
+`cd_admin` bigint(20) NOT NULL,
   `razao_social` varchar(180) NOT NULL,
   `cd_usuario` bigint(20) NOT NULL,
   `login` varchar(80) NOT NULL,
@@ -96,7 +96,7 @@ INSERT INTO `cad_admin` (`cd_admin`, `razao_social`, `cd_usuario`, `login`, `sen
 --
 
 CREATE TABLE IF NOT EXISTS `cad_curso` (
-  `cd_curso` bigint(20) NOT NULL,
+`cd_curso` bigint(20) NOT NULL,
   `desc_curso` text NOT NULL,
   `periodo_inicial` date NOT NULL,
   `cd_prof` bigint(20) NOT NULL,
@@ -122,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `cad_disciplina` (
   `cd_curso` bigint(20) NOT NULL,
   `nome_disciplina` varchar(300) NOT NULL,
   `desc_disciplina` text NOT NULL,
-  `cd_disciplina` int(11) NOT NULL
+`cd_disciplina` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
@@ -144,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `cad_horario` (
   `horario_inicial` time NOT NULL,
   `observacao` varchar(200) NOT NULL,
   `cd_curso` bigint(10) NOT NULL,
-  `cd_horario` bigint(10) NOT NULL,
+`cd_horario` bigint(10) NOT NULL,
   `data_inicial` date NOT NULL,
   `data_final` date NOT NULL,
   `horario_final` time NOT NULL
@@ -176,7 +176,7 @@ INSERT INTO `cad_horario` (`horario_inicial`, `observacao`, `cd_curso`, `cd_hora
 --
 
 CREATE TABLE IF NOT EXISTS `cad_modulo` (
-  `cd_modulo` bigint(20) NOT NULL,
+`cd_modulo` bigint(20) NOT NULL,
   `nome_modulo` varchar(180) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
@@ -201,7 +201,7 @@ CREATE TABLE IF NOT EXISTS `cad_notas` (
   `nota2` float DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
   `cd_turma` bigint(50) NOT NULL,
-  `cd_nota` int(11) NOT NULL
+`cd_nota` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
@@ -221,7 +221,7 @@ INSERT INTO `cad_notas` (`cd_aluno`, `nota1`, `nota2`, `status`, `cd_turma`, `cd
 --
 
 CREATE TABLE IF NOT EXISTS `cad_prof` (
-  `cd_prof` bigint(20) NOT NULL,
+`cd_prof` bigint(20) NOT NULL,
   `cd_usuario` bigint(20) NOT NULL,
   `razao_social` varchar(190) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
@@ -240,7 +240,7 @@ INSERT INTO `cad_prof` (`cd_prof`, `cd_usuario`, `razao_social`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `cad_submodulo` (
-  `cd_submodulo` bigint(20) NOT NULL,
+`cd_submodulo` bigint(20) NOT NULL,
   `nome_submodulo` varchar(180) NOT NULL,
   `fonte` varchar(450) NOT NULL,
   `permissao` varchar(11) NOT NULL,
@@ -258,11 +258,11 @@ INSERT INTO `cad_submodulo` (`cd_submodulo`, `nome_submodulo`, `fonte`, `permiss
 (4, 'Adiciona Aluno', 'addAluno.php', '1', 'glyphicon-user'),
 (5, 'Adiciona Submodulo', '', '1', 'glyphicon-cog'),
 (6, 'Adiciona Disciplina ', 'addDisciplina.php', '1,2', 'glyphicon-qrcode'),
-(7, 'Add Material', 'addArquivo.php', '1,2', 'glyphicon-file'),
+(7, 'Add Material', 'addArquivo.php', '1,2', 'glyphicon glyphicon-folder-open'),
 (8, 'Adiciona Frequencia', 'addfrequencia.php', '1,2', 'glyphicon-list-alt'),
-(9, 'Ver Frequencia', 'verFrequencia.php', '1,2,3', 'glyphicon glyphicon-pencil'),
-(10, 'Ver Horário', 'verHorario.php', '1,2,3', 'glyphicon glyphicon-pencil'),
-(11, 'Ver Notas', 'verNotas.php', '1,2,3', 'glyphicon glyphicon-tasks'),
+(9, 'Ver Frequencia', 'verFrequencia.php', '1,2,3', 'glyphicon glyphicon-list-alt'),
+(10, 'Ver Horário', 'verHorario.php', '1,2,3', 'glyphicon glyphicon-calendar'),
+(11, 'Ver Notas', 'verNotas.php', '1,2,3', 'glyphicon glyphicon-th'),
 (12, 'Ver Disciplina', 'verDisciplina.php', '1,2,3', 'glyphicon glyphicon-tasks'),
 (13, 'Mensagens', 'mensagens.php', '1,2,3', 'glyphicon glyphicon-envelope');
 
@@ -273,7 +273,7 @@ INSERT INTO `cad_submodulo` (`cd_submodulo`, `nome_submodulo`, `fonte`, `permiss
 --
 
 CREATE TABLE IF NOT EXISTS `cad_turma` (
-  `id` bigint(20) NOT NULL,
+`id` bigint(20) NOT NULL,
   `cd_curso` bigint(20) NOT NULL,
   `turno` varchar(40) NOT NULL,
   `cd_turma` bigint(20) NOT NULL
@@ -294,7 +294,7 @@ INSERT INTO `cad_turma` (`id`, `cd_curso`, `turno`, `cd_turma`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `frequencia` (
-  `cd_frequencia` bigint(20) NOT NULL,
+`cd_frequencia` bigint(20) NOT NULL,
   `cd_aluno` bigint(20) NOT NULL,
   `presenca` tinyint(1) NOT NULL,
   `data` date NOT NULL,
@@ -308,7 +308,7 @@ CREATE TABLE IF NOT EXISTS `frequencia` (
 --
 
 CREATE TABLE IF NOT EXISTS `mensagens` (
-  `cd_mensagem` bigint(20) NOT NULL,
+`cd_mensagem` bigint(20) NOT NULL,
   `cd_de` varchar(180) NOT NULL,
   `cd_para` varchar(180) NOT NULL,
   `assunto` text NOT NULL,
@@ -327,12 +327,12 @@ CREATE TABLE IF NOT EXISTS `noticias` (
   `tit_noticia` varchar(25) NOT NULL,
   `desc_noticia` varchar(160) NOT NULL,
   `imagem` varchar(50) DEFAULT NULL,
-  `cd_noticias` bigint(20) NOT NULL,
+`cd_noticias` bigint(20) NOT NULL,
   `permissao` varchar(1) NOT NULL,
   `posicao` varchar(10) NOT NULL,
   `categoria` varchar(20) NOT NULL,
   `foto` varchar(300) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `noticias`
@@ -342,7 +342,8 @@ INSERT INTO `noticias` (`tit_noticia`, `desc_noticia`, `imagem`, `cd_noticias`, 
 ('InscriÃ§Ãµes Abertas', 'As inscriÃ§Ãµes para cursos tÃ©cnicos iniciam dia 22/04/2016', NULL, 6, '1', '1', '1', ''),
 ('Pronatec', 'Encontra-se disponÃ­vel o edital do Pronatec.', NULL, 7, '1', '3', '2', ''),
 ('OlimpÃ­ada ', 'Entre no site do SENAI e tenha acesso a cobertura da OlimpÃ­ada do Conhecimento.', NULL, 8, '1', '1', '3', ''),
-('Te Amo Renata', ' Tudo funcionou corretamente. ', NULL, 9, '3', '1', '1', 'amor.jpg');
+('Te Amo Renata', ' Tudo funcionou corretamente. ', NULL, 9, '3', '1', '1', 'amor.jpg'),
+('Erica', ' testando!', NULL, 10, '4', '3', '2', '20151004_221820.jpg');
 
 -- --------------------------------------------------------
 
@@ -351,7 +352,7 @@ INSERT INTO `noticias` (`tit_noticia`, `desc_noticia`, `imagem`, `cd_noticias`, 
 --
 
 CREATE TABLE IF NOT EXISTS `notificacoes` (
-  `cd_notificacao` bigint(20) NOT NULL,
+`cd_notificacao` bigint(20) NOT NULL,
   `cd_usuario` bigint(20) NOT NULL,
   `tp_notificacao` varchar(80) NOT NULL,
   `desc_notificacao` text NOT NULL,
@@ -396,7 +397,7 @@ INSERT INTO `notificacoes` (`cd_notificacao`, `cd_usuario`, `tp_notificacao`, `d
 CREATE TABLE IF NOT EXISTS `seg_usuario` (
   `login` int(10) NOT NULL,
   `senha` varchar(50) NOT NULL,
-  `cd_usuario` bigint(20) NOT NULL,
+`cd_usuario` bigint(20) NOT NULL,
   `dt_sistema` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `cd_aluno` bigint(20) NOT NULL,
   `cd_prof` bigint(1) NOT NULL
@@ -425,97 +426,97 @@ INSERT INTO `seg_usuario` (`login`, `senha`, `cd_usuario`, `dt_sistema`, `cd_alu
 -- Indexes for table `arquivos`
 --
 ALTER TABLE `arquivos`
-  ADD PRIMARY KEY (`cd_arquivo`);
+ ADD PRIMARY KEY (`cd_arquivo`);
 
 --
 -- Indexes for table `cadaluno`
 --
 ALTER TABLE `cadaluno`
-  ADD PRIMARY KEY (`cd_aluno`);
+ ADD PRIMARY KEY (`cd_aluno`);
 
 --
 -- Indexes for table `cad_admin`
 --
 ALTER TABLE `cad_admin`
-  ADD PRIMARY KEY (`cd_admin`);
+ ADD PRIMARY KEY (`cd_admin`);
 
 --
 -- Indexes for table `cad_curso`
 --
 ALTER TABLE `cad_curso`
-  ADD PRIMARY KEY (`cd_curso`);
+ ADD PRIMARY KEY (`cd_curso`);
 
 --
 -- Indexes for table `cad_disciplina`
 --
 ALTER TABLE `cad_disciplina`
-  ADD PRIMARY KEY (`cd_disciplina`);
+ ADD PRIMARY KEY (`cd_disciplina`);
 
 --
 -- Indexes for table `cad_horario`
 --
 ALTER TABLE `cad_horario`
-  ADD PRIMARY KEY (`cd_horario`);
+ ADD PRIMARY KEY (`cd_horario`);
 
 --
 -- Indexes for table `cad_modulo`
 --
 ALTER TABLE `cad_modulo`
-  ADD PRIMARY KEY (`cd_modulo`);
+ ADD PRIMARY KEY (`cd_modulo`);
 
 --
 -- Indexes for table `cad_notas`
 --
 ALTER TABLE `cad_notas`
-  ADD PRIMARY KEY (`cd_nota`);
+ ADD PRIMARY KEY (`cd_nota`);
 
 --
 -- Indexes for table `cad_prof`
 --
 ALTER TABLE `cad_prof`
-  ADD PRIMARY KEY (`cd_prof`);
+ ADD PRIMARY KEY (`cd_prof`);
 
 --
 -- Indexes for table `cad_submodulo`
 --
 ALTER TABLE `cad_submodulo`
-  ADD PRIMARY KEY (`cd_submodulo`);
+ ADD PRIMARY KEY (`cd_submodulo`);
 
 --
 -- Indexes for table `cad_turma`
 --
 ALTER TABLE `cad_turma`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `frequencia`
 --
 ALTER TABLE `frequencia`
-  ADD PRIMARY KEY (`cd_frequencia`);
+ ADD PRIMARY KEY (`cd_frequencia`);
 
 --
 -- Indexes for table `mensagens`
 --
 ALTER TABLE `mensagens`
-  ADD PRIMARY KEY (`cd_mensagem`);
+ ADD PRIMARY KEY (`cd_mensagem`);
 
 --
 -- Indexes for table `noticias`
 --
 ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`cd_noticias`);
+ ADD PRIMARY KEY (`cd_noticias`);
 
 --
 -- Indexes for table `notificacoes`
 --
 ALTER TABLE `notificacoes`
-  ADD PRIMARY KEY (`cd_notificacao`);
+ ADD PRIMARY KEY (`cd_notificacao`);
 
 --
 -- Indexes for table `seg_usuario`
 --
 ALTER TABLE `seg_usuario`
-  ADD PRIMARY KEY (`cd_usuario`);
+ ADD PRIMARY KEY (`cd_usuario`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -525,82 +526,82 @@ ALTER TABLE `seg_usuario`
 -- AUTO_INCREMENT for table `arquivos`
 --
 ALTER TABLE `arquivos`
-  MODIFY `cd_arquivo` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+MODIFY `cd_arquivo` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `cadaluno`
 --
 ALTER TABLE `cadaluno`
-  MODIFY `cd_aluno` bigint(45) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=39;
+MODIFY `cd_aluno` bigint(45) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=39;
 --
 -- AUTO_INCREMENT for table `cad_admin`
 --
 ALTER TABLE `cad_admin`
-  MODIFY `cd_admin` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `cd_admin` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `cad_curso`
 --
 ALTER TABLE `cad_curso`
-  MODIFY `cd_curso` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `cd_curso` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `cad_disciplina`
 --
 ALTER TABLE `cad_disciplina`
-  MODIFY `cd_disciplina` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `cd_disciplina` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `cad_horario`
 --
 ALTER TABLE `cad_horario`
-  MODIFY `cd_horario` bigint(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+MODIFY `cd_horario` bigint(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `cad_modulo`
 --
 ALTER TABLE `cad_modulo`
-  MODIFY `cd_modulo` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `cd_modulo` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `cad_notas`
 --
 ALTER TABLE `cad_notas`
-  MODIFY `cd_nota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `cd_nota` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `cad_prof`
 --
 ALTER TABLE `cad_prof`
-  MODIFY `cd_prof` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `cd_prof` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `cad_submodulo`
 --
 ALTER TABLE `cad_submodulo`
-  MODIFY `cd_submodulo` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+MODIFY `cd_submodulo` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `cad_turma`
 --
 ALTER TABLE `cad_turma`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12345681;
+MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12345681;
 --
 -- AUTO_INCREMENT for table `frequencia`
 --
 ALTER TABLE `frequencia`
-  MODIFY `cd_frequencia` bigint(20) NOT NULL AUTO_INCREMENT;
+MODIFY `cd_frequencia` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `mensagens`
 --
 ALTER TABLE `mensagens`
-  MODIFY `cd_mensagem` bigint(20) NOT NULL AUTO_INCREMENT;
+MODIFY `cd_mensagem` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `cd_noticias` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `cd_noticias` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `notificacoes`
 --
 ALTER TABLE `notificacoes`
-  MODIFY `cd_notificacao` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
+MODIFY `cd_notificacao` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `seg_usuario`
 --
 ALTER TABLE `seg_usuario`
-  MODIFY `cd_usuario` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `cd_usuario` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
