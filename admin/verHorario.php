92052b3838cba2fb03dc790cd2be5f8d43a23537
Eliseu dos Santos <s.eliseu.santos@gmail.com>
2016-05-02 07:18:13 -0300

    Finaliza tela de ver horario

--- admin/verHorario.php
+++ admin/verHorario.php
@@ -1,8 +1,7 @@
-<?php 
-  include '../classes/funcoes.class.php'; 
-  include '../classes/aluno.class.php'; 
-  include '../classes/notas.class.php'; 
-   include '../classes/curso.class.php'; 
+<?php
+  include '../classes/funcoes.class.php';
+  include '../classes/curso.class.php';
+  include '../classes/horario.class.php';
   Funcoes::geraHeader();
   Funcoes::geraMenus();
 ?>
@@ -18,39 +17,44 @@
                   if($dados->cd_curso == $curso->cd_curso) :
             ?>
                     <option value="<?= $curso->cd_curso ?>" selected><?= $curso->nome_curso; ?></option>
-            <?php 
+            <?php
                   else :
             ?>
                   <option value="<?= $curso->cd_curso ?>"><?= $curso->nome_curso; ?></option>
-            <?php 
-                  endif; 
-                endforeach; 
+            <?php
+                  endif;
+                endforeach;
             ?>
           </select>
     </div>
   </div>
   <div class="row">
     <div class="col-md-12">
+     <?php
+        $data = date("d/m/Y");
+        $objetoHorari = new Horario();
+        $alunos = $objetoHorari->getHorariosPorAluno($_SESSION['cd_aluno']);
+      ?>
       <table class="table table-bordered">
         <thead>
           <tr>
-            <td width="200" class="text-center"> Data </td> 
-            <td width="150" class="text-center"> Horário </td> 
-            <td width="450" class="text-center"> Componente </td> 
+            <td width="200" class="text-center"> Data </td>
+            <td width="250" class="text-center"> Horário </td>
+            <td width="450" class="text-center"> Componente </td>
           </tr>
         </thead>
         <tbody>
-          <?php for($i = 0; $i < 12; $i++) : ?>
+          <?php foreach ($alunos as $aluno) : ?>
             <tr>
-               <td>&nbsp;</td>  
-               <td>&nbsp;</td>
-               <td>&nbsp;</td> 
+              <td class="text-center"><?= date('d/m/Y', strtotime($aluno->data_inicial)) . ' à ' . date('d/m/Y', strtotime($aluno->data_final)); ?></td>
+              <td class="text-center"><?= date('H:m', strtotime($aluno->horario_inicial)) . ' à ' . date('H:m', strtotime($aluno->horario_final)); ?></td>
+              <td class="text-center"><?= $aluno->desc_curso; ?></td>
             </tr>
-          <?php endfor; ?>
+          <?php endforeach; ?>
         </tbody>
       </table>
-    </div>  
-    <div class="col-md-6"> 
+    </div>
+    <div class="col-md-6">
       <input type="button"  name="imprimir"   class="btn btn-primary btn-block" value="Imprimir" onclick="window.print()";>
     </div>
   </div>
--- classes/horario.class.php
+++ classes/horario.class.php
@@ -8,7 +8,14 @@ Class Horario extends Funcoes {
   }
 
   public function getHorarios($cd_curso) {
-  	$objetoSql = new Database();	
-  	$objetoSql->query('select * from cad_horario where cd_curso = {$cd_curso}');
+  	$objetoSql = new Database();
+  	$resuldado = $objetoSql->query('select * from cad_horario where cd_curso = '.$cd_curso.'')->result();
+    return $resultado;
+  }
+
+    public function getHorariosPorAluno($cd_aluno) {
+    $objetoSql = new Database();
+    $resultado = $objetoSql->query('select * from cad_horario inner join cad_turma using(cd_curso) inner join cadaluno using(cd_turma) inner join cad_curso using(cd_curso) where cd_aluno = '.$cd_aluno.'')->result();
+    return $resultado;
   }
 }
